68e3c847ed02073d5a1f31d5945077fde9ac50a4
shahrukh <50910055+Tilwa@users.noreply.github.com>
2021-10-04 06:45:29 +0500

    removing local State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,35 +2,41 @@ import React, { Component } from 'react';
 
 class Counter extends React.Component {
     state = {
-        count: 0,
+        count: this.props.counter.value,
         tags: ["tag1", "tag2", "tag3"]
     };
 
-    styles = {
-        fontSize: 20,
-        fontWeight: "bold"
-    };
+    // styles = {
+    //     fontSize: 20,
+    //     fontWeight: "bold"
+    // };
 
     // constructor() {
     //     super();
     //     this.handleIncrement = this.handleIncrement.bind(this);
     // }
-
-    handleIncrement = () => {
-        console.log("button clicked", this);
+ 
+    handleIncrement = product => {
+        console.log(product);
+        this.setState({ count: this.state.count + 1});
     }
     render() { 
+        console.log("props", this.props);
         return (
             <div>
-                
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button 
-                onClick={this.handleIncrement}
-                className="btn btn-secondary btn-sm">Increment</button>
+                onClick={() => this.handleIncrement(this.state.tags[2])}
+                className="btn btn-secondary btn-sm"
+                >Increment</button>
                 
-                <ul>
+                <button 
+                className="btn btn-danger btn-sm m-2"
+                onClick={() => this.props.onDelete(this.props.counter.id)}
+                >Delete</button>
+                {/* <ul>
                     {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                </ul> */}
             </div>
         );
     }
