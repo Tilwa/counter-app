25f231335c327abae92a51cbe358f987d5e8ccdc
shahrukh <50910055+Tilwa@users.noreply.github.com>
2021-10-06 07:12:17 +0500

    hooks

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 class Counter extends React.Component {
   render() {
     return (
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,16 @@
-import React, { Component } from "react";
-
-class Navbar extends React.Component {
-  render() {
-    return <div></div>;
-  }
-}
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
